cdaf828ab9ee1f29e706cb7df7567a7c3328bad7
Fahad Bashir <fahadbashir757@gmail.com>
2024-10-27 05:07:46 +0500

    init push

---
+++ helpers/recaptchaHelper.js
@@ -0,0 +1,15 @@
+async function verifyRecaptcha(captchaValue) {
+  const secretKey = process.env.RECAPTCHA_SECRET_KEY; // Use the environment variable
+  const response = await fetch('https://www.google.com/recaptcha/api/siteverify', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: `secret=${secretKey}&response=${captchaValue}`,
+  });
+
+  const data = await response.json();
+  return data.success;
+}
+
+module.exports = { verifyRecaptcha }; // Export using CommonJS
